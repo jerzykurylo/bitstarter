a6baac89f0bfbfc39dfc77a3edd03ee2232c517d
Jerzy Kurylo <jerzy.kurylo@wp.pl>
2013-07-11 21:48:26 +0000

    web.js

--- web.js
+++ web.js
@@ -1,8 +1,21 @@
+/*var express = require('express');
 
+var app = express.createServer(express.logger());
 
-var fs = require('fs');
-fs.readFile('index.html', function (err, data) {
-	if (err) throw err;
-	console.log(data);
+app.get('/', function(request, response) {
+  response.send('Hello World 2!');
 });
-q
+*/
+
+
+var = fs = require('fs');
+fs.readFile('index.html');
+
+
+
+
+/*var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+*/
