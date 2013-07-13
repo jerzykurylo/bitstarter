cbe0b7cf115a9081a43804ea29ba2cfef92e9dd8
Jerzy Kurylo <jerzy.kurylo@wp.pl>
2013-07-13 05:23:00 +0000

    web.js

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+
+  fs.readFile('index.html', function(err, contents)  {
+    response.write(contents);
+    response.end();
+      });
+
 });
 
 
